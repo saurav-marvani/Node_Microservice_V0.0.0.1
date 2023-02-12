b6f7e3da59c0886cdf4ce7154b1313c2e0e32e9a
saurav-r-github <michealvinamra@gmail.com>
2023-02-12 18:16:08 +0530

    server.js

--- resource-service/server.js
+++ resource-service/server.js
@@ -44,4 +44,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}👋👋👋👋👋`));
